6dac9274958ca6dba63cff948c423c7e1b1fff4c
enginyilmz <enginyilmaz.math@hotmail.com>
2017-02-14 11:59:14 +0300

    AddRequiredFieldToCustomer

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -19,6 +19,7 @@ namespace Vidly.Models
         public MembershipType MembershipType { get; set; }
 
         [Display(Name = "Membership Type")]
+        [Required]
         public byte MembershipTypeId { get; set; }
         
         [Display(Name = "Date of Birth")]
