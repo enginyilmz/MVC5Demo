d9b4cae4b2acf6e08ac9358229ef687ffe14327c
enginyilmz <enginyilmaz.math@hotmail.com>
2017-02-15 11:39:24 +0300

    Add custom validation to customer field.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -23,6 +23,7 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; }
         
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
 
